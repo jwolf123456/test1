079de300773624fce132671dc66bc7e2300c3214
j_maj <523083921@qq.com>
2020-04-12 01:55:49 +0800

    first commit

---
+++ src/main/java/com/demo20200412/demo20200412/Demo20200412Application.java
@@ -0,0 +1,13 @@
+package com.demo20200412.demo20200412;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20200412Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo20200412Application.class, args);
+    }
+
+}
