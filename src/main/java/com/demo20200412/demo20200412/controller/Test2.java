9bb8127b57981e6ed82babf1d3345a09cf0a7022
j_maj <523083921@qq.com>
2020-04-12 02:18:50 +0800

    a

---
+++ src/main/java/com/demo20200412/demo20200412/controller/Test2.java
@@ -0,0 +1,13 @@
+package com.demo20200412.demo20200412.controller;
+
+/**
+ * <p>
+ * Description: TODO
+ * </p>
+ *
+ * @author majun
+ * @version 1.0
+ * @date 2020-04-12 02:15
+ */
+public class Test2 {
+}
